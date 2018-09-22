459ca8a16c30e268d5826b8fa4197192545f3048
Yuanxin Wang <y2469wan@uwaterloo.ca>
2018-09-22 18:06:59 +0800

    Assignment 3 Finished

--- Assignments/machine-learning-ex3/ex3/lrCostFunction.m
+++ Assignments/machine-learning-ex3/ex3/lrCostFunction.m
@@ -1,14 +1,14 @@
 function [J, grad] = lrCostFunction(theta, X, y, lambda)
-%LRCOSTFUNCTION Compute cost and gradient for logistic regression with 
+%LRCOSTFUNCTION Compute cost and gradient for logistic regression with
 %regularization
 %   J = LRCOSTFUNCTION(theta, X, y, lambda) computes the cost of using
 %   theta as the parameter for regularized logistic regression and the
-%   gradient of the cost w.r.t. to the parameters. 
+%   gradient of the cost w.r.t. to the parameters.
 
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -25,24 +25,33 @@ grad = zeros(size(theta));
 %
 %       Each row of the resulting matrix will contain the value of the
 %       prediction for that example. You can make use of this to vectorize
-%       the cost function and gradient computations. 
+%       the cost function and gradient computations.
 %
-% Hint: When computing the gradient of the regularized cost function, 
+% Hint: When computing the gradient of the regularized cost function,
 %       there're many possible vectorized solutions, but one solution
 %       looks like:
 %           grad = (unregularized gradient for logistic regression)
-%           temp = theta; 
-%           temp(1) = 0;   % because we don't add anything for j = 0  
+%           temp = theta;
+%           temp(1) = 0;   % because we don't add anything for j = 0
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
 
+reg_theta = theta(2:size(theta));
+htheta = sigmoid(X*theta);
+J = (1/m)*sum(-y.*log(htheta) - (1 - y).*(log(1 - htheta))) + (lambda/(2*m))*sum(reg_theta.*reg_theta);
+
+% Follow the instruction, we use a temp here, NOT for the simultaneous update rule
+% But for the rule that theta0 does not participate in regularized gradient descent
+
+% temp = theta;
+% temp(1) = 0;
+% grad = (1/m)*transpose(X)*(htheta - y) + (lambda/m)*temp;
 
 
-
-
-
-
+% This is an alternative solution
+grad = (1/m)*transpose(X)*(htheta - y) + (lambda/m)*theta;
+grad(1) = (1/m)*sum((htheta - y).* X(:, 1));
 
 
 % =============================================================
